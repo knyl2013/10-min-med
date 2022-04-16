c1ad729d51b91e75488ed67a724b86055f35887e
LiWaiYip <1155115790@link.cuhk.edu.hk>
2022-04-16 17:04:48 +0800

    Side menu bar & setting screen

---
+++ constants/DrawerItems.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    name: "Meditation",
+    iconType: "Material",
+    iconName: "face-profile",
+  },
+  {
+    name: "Settings",
+    iconType: "Feather",
+    iconName: "settings",
+  },
+];
