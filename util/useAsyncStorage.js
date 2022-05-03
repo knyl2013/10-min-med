2b9209af891d2519af3333a4989b25fb6b23af93
waiyip <waiyip@waiyips-MacBook-Air.local>
2022-05-03 20:16:07 +0800

    Update AsyncStorage

--- util/useAsyncStorage.js
+++ util/useAsyncStorage.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { AsyncStorage } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 export function useAsyncStorage(key, initialValue) {
   const [storedValue, setStoredValue] = useState();
 
