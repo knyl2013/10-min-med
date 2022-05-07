ec81452caff70c012cde52ae26afde5b97a07711
waiyip <waiyip@waiyips-MacBook-Air.local>
2022-05-07 09:35:50 +0800

    Improve getConsecutiveDays()

--- screens/Meditation.js
+++ screens/Meditation.js
@@ -142,21 +142,18 @@ export default function MeditationScreen() {
     if (!days || !days.length) return 0;
     const daysObj = JSON.parse(days);
     if (!daysObj || !daysObj.length) return false;
+    const n = daysObj.length;
     let ans = isTodayDone(days) ? 1 : 0;
     let target = getYesterday();
-    let n = daysObj.length;
-    while (n--) {
-      let targetStr = target.toString();
-      let found = false;
-      for (const day of daysObj) {
-        if (day == targetStr) {
-          ans++;
-          found = true;
-          target = getYesterdayOf(target);
-          break;
-        }
-      }
-      if (!found) break;
+    var set = new Set();
+    for (const day of daysObj) {
+      set.add(day);
+    }
+    for (let i = 0; i < n; i++) {
+      if (set.has(target.toString())) {
+        ans++;
+        target = getYesterdayOf(target);
+      } else break;
     }
     return ans;
   };
