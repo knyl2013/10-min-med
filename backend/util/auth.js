98a4d09f878454f77039e988f7e41643888b85eb
waiyip <waiyip@waiyips-MacBook-Air.local>
2022-05-16 19:49:39 +0800

    Update token expire day

--- backend/util/auth.js
+++ backend/util/auth.js
@@ -1,10 +1,10 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 function generateToken(userInfo) {
   if (!userInfo) {
     return null;
   }
   return jwt.sign(userInfo, process.env.JWT_SECRET, {
-    expiresIn: '1h'
+    expiresIn: "60d",
   });
 }
 
@@ -13,22 +13,22 @@ function verifyToken(email, token) {
     if (err) {
       return {
         verified: false,
-        message: 'invalid token'
+        message: "invalid token",
       };
     }
 
     if (res.email != email) {
       return {
         verified: false,
-        message: 'invalid email'
+        message: "invalid email",
       };
     }
 
     return {
       verified: true,
-      message: 'verified'
-    }
-  })
+      message: "verified",
+    };
+  });
 }
 
 module.exports.generateToken = generateToken;
